b45d26a5247fe4c3ab8f3d49d5b649b41c36b0ac
Emily <113935577+WeiEmily@users.noreply.github.com>
2023-04-17 17:10:56 +0100

    merge

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors")
 require("dotenv").config()
 
 var indexRouter = require('./routes/index');
@@ -24,6 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
